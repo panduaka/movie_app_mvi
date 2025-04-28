58e393b695d1a2eef711fa44919178cd3df9b8da
panduka.wedisinghe <panduka1991@gmail.com>
2025-04-28 11:45:50 +0100

    Refactor Repository Module

--- app/src/main/java/com/example/movieapp/di/repository_module.kt
+++ app/src/main/java/com/example/movieapp/di/repository_module.kt
@@ -6,15 +6,27 @@ import com.example.movieapp.data.remote_service.MovieApiService
 import com.example.movieapp.domain.repository.MovieRepository
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
+//@Module
+//@InstallIn(SingletonComponent::class)
+//abstract class RepositoryModule {
+//
+//    @Binds
+//    @Singleton
+//    abstract fun bindMovieRepository(movieRepositoryImpl: MovieRepositoryImpl): MovieRepository
+//}
+
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class RepositoryModule {
+object  RepositoryModule {
 
-    @Binds
+    @Provides
     @Singleton
-    abstract fun bindMovieRepository(movieRepositoryImpl: MovieRepositoryImpl): MovieRepository
+    fun provideMovieRepository(movieApiService: MovieApiService): MovieRepository {
+        return MovieRepositoryImpl(movieApiService)
+    }
 }
